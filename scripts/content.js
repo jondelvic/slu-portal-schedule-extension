505d89344ef3f4bc3d5c3238c7beba0b3a051076
Jon Delvic Calub <2240314@slu.edu.ph>
2025-06-06 15:29:30 +0800

    Tentative basic CSV formatting from Class Schedule table

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,21 @@
 
 const table = document.querySelectorAll("table.mws-table");
 const thead = document.querySelectorAll("table.mws-table > thead > tr > th"); // pwede pala ito
-const tbody = document.getElementsByTagName("tbody");
+const tbody = document.getElementsByTagName("tbody"); 
 
 // Class Schedule Table (index 1 is for the Weekly View Table)
-console.log(table[0]); 
-console.log(thead); 
-console.log(tbody[0]);
+// console.log(table[0]); 
+// console.log(thead); 
+// console.log(tbody[0]);
+
+// get headers
+const tableHeaders = [];
+for (let i = 0; i < thead.length; i++) {
+    // console.log(thead.item(i).innerText);
+    tableHeaders.push(thead.item(i).innerText);
+}
+
+console.log(tableHeaders); // just realized that i might not be needing the headers after all lolz (unless maybe i implement json conversion)
 
 // get number of subjects
 // Count the number of <tr> elements in <tbody> - 1 (total units)
@@ -15,11 +24,12 @@ const tbodyRows = tbody[0].rows;
 const courseCount = tbodyRows.length - 1;
 console.log("Number of courses enrolled: " + courseCount);
 
-// get headers
-const tableHeaders = [];
-
-console.log("TABLE HEADERS:")
-for (let i = 0; i < thead.length; i++) {
-    console.log(thead.item(i).innerText);
-    // tableHeaders.push(thead.children[i].innerText);
+// get data 
+console.log("Schedule details of enrolled courses: ")
+for (let i = 0; i < courseCount; i++) {
+    console.log(tbodyRows.item(i).innerText.replaceAll(/\s/g,',')); // remove whitespaces and replace with comma
 }
+
+// .csv PROCESSING 
+
+// .ics PROCESSING
