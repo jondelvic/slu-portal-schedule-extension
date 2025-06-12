743056a76554bbad8c577fdbb35a7d579c796ac3
Jon Delvic Calub <2240314@slu.edu.ph>
2025-06-12 13:32:10 +0800

    Data collection for CSV processing per header array

--- scripts/content.js
+++ scripts/content.js
@@ -1,66 +1,70 @@
+// Read class schedule table from schedule tab of portal
 const table = document.querySelectorAll("table.mws-table");
-const thead = document.querySelectorAll("table.mws-table > thead > tr > th"); // pwede pala ito
+const thead = document.querySelectorAll("table.mws-table > thead > tr > th"); 
 const tbody = document.getElementsByTagName("tbody"); 
 
-// Class Schedule Table (index 1 is for the Weekly View Table)
-// console.log(table[0]); 
-// console.log(thead); 
-// console.log(tbody[0]);
+// Get class schedule table headers
+const tableHeaders = [];
+for (let i = 0; i < thead.length; i++) {
+    tableHeaders.push(thead.item(i).innerText);
+}
 
-// get number of subjects
-// Count the number of <tr> elements in <tbody> - 1 (total units)
+// Count number of courses enrolled
 const tbodyRows = tbody[0].rows;
 const courseCount = tbodyRows.length - 1;
 console.log("Number of courses enrolled: " + courseCount);
 
-// get data 
-const courseScheduleDetails = [];
+// Get course schedule data
 console.log("Schedule details of enrolled courses: ")
+
+const courseScheduleDetails = [];
 for (let i = 0; i < courseCount; i++) {
-    console.log(tbodyRows.item(i).innerText.replaceAll(/\t/g,',')); // remove tab whitespace and replace with comma
-    courseScheduleDetails.push(tbodyRows.item(i).innerText.replaceAll(/\t/g,','));
+    let courseSchedule = tbodyRows.item(i).innerText.replaceAll(/\t/g,',');
+    courseSchedule = courseSchedule.substring(0, courseSchedule.length - 1);
+
+    console.log(courseSchedule);
+
+    courseScheduleDetails.push(courseSchedule);
 }
 
-// get headers
-const tableHeaders = [];
-for (let i = 0; i < thead.length; i++) {
-    // console.log(thead.item(i).innerText);
-    tableHeaders.push(thead.item(i).innerText);
-}
-
-console.log(tableHeaders); // just realized that i might not be needing the headers after all lolz (unless maybe i implement json conversion)
-
-
-console.log(courseScheduleDetails);
-
-/* NOTES:
-    - If marked as DAILY, event frequency should be monday to saturday
-*/
-
-// Variables for schedule details
-const eventTitle = "";
-const startDate = "";
-const endDate = "";
-const startTime = "";
-const endTime = "";
-const description = "";
-const room = "";
-
-// function for .csv processing
+// CSV Processing (.csv)
 function exportToCSV() {
     const csvHeaders = ["Subject", "Start Date", "End Date", "Start Time", "End Time", "Description", "Location"];
 
+    let subject = [];
+    let startDate = [];
+    let endDate = [];
+    let startTime = [];
+    let endTime = [];
+    let description = [];
+    let room = [];
+
     let csvContent = csvHeaders.join(",");
 
-    // test appending of current unrefined schedule details
-    for (let i = 0; i < courseCount; i++) {
-        csvContent += "\n";
-        csvContent += courseScheduleDetails.join(",");
+    for (let i = 0; i < courseScheduleDetails.length; i++) {
+        let scheduleElements = courseScheduleDetails[i].split(",");
+
+        subject.push(scheduleElements[1]);
+
+        // this could have an issue with am/pm overlap
+        let startTimeString = scheduleElements[4].substring(0, 2) + ":" + scheduleElements[4].substring(2, 4) + " " + scheduleElements[4].substring(12, 14); 
+        startTime.push(startTimeString);
+
+        let endTimeString = scheduleElements[4].substring(7, 9) + ":" + scheduleElements[4].substring(9, 11) + " " + scheduleElements[4].substring(12, 14);
+        endTime.push(endTimeString);
+
+        description.push(scheduleElements[2]);
+
+        room.push(scheduleElements[6]);
     }
 
-    console.log(csvContent);
+    console.log(subject);
+    console.log(startTime);
+    console.log(endTime);
+    console.log(description);
+    console.log(room);
 }
 
 exportToCSV();
 
-// function for .ics processing
+// TODO: iCalendar Processing (.ics)
