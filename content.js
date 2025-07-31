71dc8e5c7262255cb717d92f7137e642c0cafa22
Jon Delvic Calub <2240314@slu.edu.ph>
2025-07-31 22:06:17 +0800

    Append user's name to export file name, retrieve the schedule of the day's index to check if it will be included in the CSV file

--- content.js
+++ content.js
@@ -16,51 +16,48 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
-// AY 2024 - 2025 (this must be updated per AY)
-let firstSemEndDate = "12/18/2025" // first semester
-let secondSemEndDate = "05/23/2026" // second semester
-let shortTermEndDate = "07/22/2026" // short term
+// AY 2025 - 2026 (UPDATE THIS EVERY YEAR)
+let firstSemEndDate = "12/18/2025" 
+let secondSemEndDate = "05/23/2026" 
+let shortTermEndDate = "07/22/2026"
 
-// Read class schedule table from schedule tab of portal
+// Read class schedule tables from schedule tab of portal
 const table = document.querySelectorAll("table.mws-table");
 const thead = document.querySelectorAll("table.mws-table > thead > tr > th"); 
 const tbody = document.getElementsByTagName("tbody"); 
 
-const currentDate = new Date().toLocaleDateString(); // Get current date
+const user = document.querySelector("#mws-username"); // User's name for file name downloading
+console.log("Hello, " + user.innerText + "!");
 
-const currentDayOfTheWeek = new Date().getDay(); // Get current day (0 - 6; Sunday - Saturday)
+const currentDate = new Date().toLocaleDateString(); 
 
-const validDays = ['M', 'T', 'W', 'TH', 'F', 'S']; // Days distinguisher at portal
+const currentDayOfTheWeek = new Date().getDay(); // (0 - 6; Sunday - Saturday)
 
-// Get class schedule table headers
+const validDays = ['M', 'T', 'W', 'TH', 'F', 'S']; // Days distinguisher at portal; not really needed
+
+// Class Schedule Table Headers
 const tableHeaders = [];
 for (let i = 0; i < thead.length; i++) {
     tableHeaders.push(thead.item(i).innerText);
 }
 
-// Count number of courses enrolled
+// Number of Courses Enrolled
 const tbodyRows = tbody[0].rows;
 const courseCount = tbodyRows.length - 1;
 console.log("Number of courses enrolled: " + courseCount);
 
-// Get course schedule data
+// Course Schedule Information
 console.log("Schedule details of enrolled courses: ")
 const courseScheduleDetails = [];
 
 for (let i = 0; i < courseCount; i++) {
     let courseSchedule = tbodyRows.item(i).innerText.replaceAll(/\t/g,',');
     courseSchedule = courseSchedule.substring(0, courseSchedule.length - 1);
+    courseScheduleDetails.push(courseSchedule);
 
     console.log(courseSchedule);
-
-    courseScheduleDetails.push(courseSchedule);
 }
 
-// console.log(courseScheduleDetails);
-
-/* TODO: Check the schedule under the DAYS header from the portal if it is for this day. 
-If not, don't process for csv and only append the one that's scheduled 
-(e.g., THFS (Thursday, Friday, Saturday), DAILY (Monday to Saturday),  TTHS (Tuesday, Thursday, Saturday)) */
 // CSV Processing
 function exportToCSV() {
     const csvHeaders = ["Subject", "Start Date", "Start Time", "End Time", "Description", "Location"];
@@ -72,7 +69,7 @@ function exportToCSV() {
         startDate = "0" + currentDate;
     }
 
-    // let endDate = shortTermEndDate; // note: removed end date due to csv not supporting recurring events
+    // let endDate = shortTermEndDate; // Removed end date due to CSV not supporting recurring events
     let startTime = [];
     let endTime = [];
     let description = [];
@@ -82,61 +79,78 @@ function exportToCSV() {
     let csvContent = csvHeaders.join(",");
 
     for (let i = 0; i < courseScheduleDetails.length; i++) {
+        let isScheduledToday = false; // Boolean value if this subject is scheduled today
         csvContent += "\n";
 
         let scheduleElements = courseScheduleDetails[i].split(",");
 
         dayOfTheWeek.push(scheduleElements[5]); // Days column on portal
 
-        // tester onli:
+        // TESTER FOR WEEKLY SCHEDULE
         // dayOfTheWeek[0] = 'TTHS';
-        // dayOfTheWeek[1] = 'THFS';
-        // dayOfTheWeek[2] = 'MWTH';
+        // dayOfTheWeek[0] = 'THFS';
+        // dayOfTheWeek[0] = 'MWTH';
         // dayOfTheWeek[0] = 'MWTHFS';
         // dayOfTheWeek[0] = 'MTWTHF';
 
         let days = dayOfTheWeek[i];
-        // console.log(days);
 
         let weeklySchedule = [];
-        if (days == "DAILY") { // If this is DAILY, export immediately since it doesn't matter anyway (CSV is only for single-day exporting)
+        let weeklyScheduleIndex = [];
+        if (days == "DAILY") { // If schedule is daily, include in CSV file if file is exported during monday to saturday
             weeklySchedule = validDays;
-        } else { // If schedule is not DAILY, extract the schedule string and export only if the weekly schedule of that subject is today/tomorrow?
+            weeklyScheduleIndex = [1, 2, 3, 4, 5, 6]; // 1-6 (Monday to Saturday)
+        } else { // If schedule is not DAILY, extract the schedule string and export only if the weekly schedule of that subject is today
             weeklySchedule += days;
             weeklySchedule = weeklySchedule.split('');
 
             for (let j = 0; j < weeklySchedule.length; j++) {
                 if (weeklySchedule[j] == 'T' && weeklySchedule[j + 1] == 'H') {
                     weeklySchedule.push(weeklySchedule[j] + weeklySchedule[j + 1]);
+                    weeklyScheduleIndex.push(4);
                     
-                    if (weeklySchedule.includes('TH')) { 
+                    if (weeklySchedule.includes('TH')) { // Remove 'T' and 'H' 
                         weeklySchedule.splice(weeklySchedule.indexOf('T'), 1);
                         weeklySchedule.splice(weeklySchedule.indexOf('H'), 1);
                     } 
                 }
+
+                switch (weeklySchedule[j]) {
+                    case 'M':
+                        weeklyScheduleIndex.push(1);
+                        break;
+                    case 'T':
+                        weeklyScheduleIndex.push(2);
+                        break;
+                    case 'W':
+                        weeklyScheduleIndex.push(3);
+                        break;
+                    case 'F':
+                        weeklyScheduleIndex.push(5);
+                        break;
+                    case 'S': // SATURDAY (Sundays for graduate students are not yet handled)
+                        weeklyScheduleIndex.push(6);
+                        break;
+                } 
             }
+
+            console.log("Today is a " + currentDayOfTheWeek);
         }
 
         console.log(weeklySchedule);
+        console.log(weeklyScheduleIndex);
 
         subject.push(scheduleElements[1] + " (" + scheduleElements[0] + ")"); // <Course Number> (<Class Code>)
         csvContent += subject[i] + ",";
 
         csvContent += startDate + ","; // Date today 
 
-        // VALID AM TIME: 07:30, 8:00, 8:30, 9:00, 9:30, 10:00, 10:30, 11:30 
-        // VALID PM TIME: 12:00, 12:30, 1:30, 2:00, 2:30, 3:00, 3:30, 4:00, 4:30, 5:00, 5:30, 6:00, 6:30, 7:00, 7:30, 8:00, 8:30
         let startTimeString = scheduleElements[4].substring(0, 2) + ":" + scheduleElements[4].substring(2, 4) + " ";
 
-        // Bruteforce condition for am/pm overlap
-        // Algorithm:
-        //  - Check if the indicated time schedule is AM/PM
-        //      a. If PM, verify if there is an AM/PM overlap
-        //      b. If AM, it shouldn't have an AM/pm overlap
         if (scheduleElements[4].substring(12, 14) == "PM") {
             // console.log("This is either AM + PM schedule or purely PM");
-
-            if ((scheduleElements[4].substring(0, 2) + scheduleElements[4].substring(2,4)) < 1200 && (scheduleElements[4].substring(0, 2) + scheduleElements[4].substring(2,4)) > 830) {
+            if ((scheduleElements[4].substring(0, 2) + scheduleElements[4].substring(2,4)) < 1200 && 
+            (scheduleElements[4].substring(0, 2) + scheduleElements[4].substring(2,4)) > 830) {
                 // console.log(startTimeString + "is AM");
                 startTimeString += "AM";
             } else {
@@ -171,7 +185,7 @@ function exportToCSV() {
         const a = document.createElement('a');
 
         a.href = csvURL;
-        a.download = 'slu-portal-schedule.csv';
+        a.download = user.innerText + '_slu-portal-schedule.csv';
 
         a.click();
     }
@@ -184,4 +198,9 @@ function exportToCSV() {
 // TODO: iCalendar Processing (.ics)
 function exportToICS() {
 
+}
+
+// TODO: JSON Processing (.json)
+function exportToJSON() {
+    
 }
